3d4251d9a69134dd5860e78ed6dd4c84a4997faa
wekbor <364102504@qq.com>
2020-10-06 01:12:12 +0800

    init

--- dist/test.e98b79dd.js
+++ dist/test.e98b79dd.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59108" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64105" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
